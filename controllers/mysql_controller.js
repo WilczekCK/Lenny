43919e0ebb2de8bc38608c510a80e257d4afc4e9
Paweł Wilk <wilkuwdr2008@gmail.com>
2019-11-22 13:35:35 +0100

    auth, connection and get querie

--- controllers/mysql_controller.js
+++ controllers/mysql_controller.js
@@ -1,10 +1,48 @@
-const mysql = require('koa-mysql');
+const mysql = require('mysql2/promise');
 
 var mysql_controller = mysql_controller || {}
 mysql_controller = {
-    connect: function(){
-        console.log('connected!');
+    auth: {
+        user: 'root',
+        password: 'rootpass',
+        database: 'memePage',
+        host: 'localhost'
+    },
+    connection: () => mysql.createConnection({
+        user: 'root',
+        password: 'rootpass',
+        database: 'memePage',
+        host: 'localhost',
+    }),
+    queries: {
+        get: async (query) => {
+            const connection = await mysql_controller.connection();
+            const [rows, fields] = await connection.execute(query)
+
+            return rows;
+        }
     }
 }
+
+
+/*
+    STRUCTURE:
+        Table: Images
+            ID - INT
+            Author_ID - INT
+            Added_in - Date
+            Name - Text
+            Tags - Text
+            Likes - INT
+            Dislikes - INT
+
+        Table: Users
+            ID - INT
+            Ingame_ID - INT
+            Registered - DATE
+            SumLikes - INT
+            SumDislikes - INT
+            Role - INT
+*/
 
 module.exports = mysql_controller || 'There is a problem with a MySQL controller file';
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ module.exports = ({ homepageRoute }) => {
     const mysql = require('../controllers/mysql_controller');
     
     homepageRoute.get('/', async (ctx, next) => {
-        mysql.connect();
+        console.log(await mysql.queries.get('SELECT * FROM images where id = 10'))
         await ctx.render('index', true)
     });
 }
