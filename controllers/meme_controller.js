570de9dbd61222243426fbaf0f6ae6cce5a0273d
Paweł Wilk <wilkuwdr2008@gmail.com>
2019-12-16 17:36:08 +0100

    changed the location of uploading code, right to the controller

--- controllers/meme_controller.js
+++ controllers/meme_controller.js
@@ -1,6 +1,18 @@
+const mysql = require('../controllers/mysql_controller');
+const fs = require('fs')
+
 var meme_controller = meme_controller || {}
 meme_controller = {
-
+    insertToDB: async (author_id, date, tags) => {
+        console.log(tags)
+        const uploadedSqlID = await mysql.insert(`images`, `author_id, added_in, tags`, `${author_id}, '${date}', '${tags}'`);
+        return uploadedSqlID;
+    },
+    changeImageName: async (oldName, newName) => {
+        fs.rename(`./uploads/${oldName}`, `./uploads/${newName}.jpg`, (err) => {
+            if (err) throw err;
+        });
+    }
 }
 
 
--- routes/meme.js
+++ routes/meme.js
@@ -1,10 +1,10 @@
-var mysql = require('../controllers/mysql_controller');
+const mysql = require('../controllers/mysql_controller');
+const meme = require('../controllers/meme_controller');
 const session = require('koa-session');
 const koaBody = require('koa-body');
 const multer = require('@koa/multer');
 const upload = multer({ dest: './uploads/' });
 const moment = require('moment')
-const fs = require('fs')
 
 module.exports = ({ memeRoute }) => {
     memeRoute.get('/', async (ctx, next) => {
@@ -13,28 +13,26 @@ module.exports = ({ memeRoute }) => {
 
     memeRoute.get('/add', async (ctx, next) => {
         const is_player_logged = ctx.req.body[0];
-        if(!is_player_logged) ctx.throw(400, 'User is not logged in');
-        await ctx.render('newMeme', {userInfo: is_player_logged});
+        if (!is_player_logged) ctx.throw(400, 'User is not logged in');
+        await ctx.render('newMeme', { userInfo: is_player_logged });
     })
 
-    memeRoute.post('/add',  upload.fields([
+    memeRoute.post('/add', upload.fields([
         {
             name: 'meme',
             maxCount: 1
         },
         {
-            name:'tags',
+            name: 'tags',
             maxCount: 1
         }
     ]), async (ctx, next) => {
-        if(!ctx.request) return ctx.throw(400, 'One of the fields is missing');
-        const {filename} = ctx.request.files.meme[0]; //image-id
-        const {tags, author_id} = ctx.request.body; 
+        if (!ctx.request) return ctx.throw(400, 'One of the fields is missing');
+        const { filename } = ctx.request.files.meme[0]; //image-id
+        const { tags, author_id } = ctx.request.body;
 
-        const uploadedSqlID = await mysql.insert(`images`, `author_id, added_in, tags`, `${author_id}, '${moment().format('YYYY-MM-DD')}', '${tags}'`);
-        fs.rename(`./uploads/${filename}`, `./uploads/${uploadedSqlID}.jpg`, (err) => {
-            if (err) throw err;
-        });
+        const uploadedSqlID = await meme.insertToDB(`${author_id}`, `${moment().format('YYYY-MM-DD')}`, `${tags}`)
+        await meme.changeImageName(`${filename}`, `${uploadedSqlID}`);
 
         await ctx.redirect('/');
         await next();
