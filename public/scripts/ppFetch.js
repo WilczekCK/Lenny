fe4ed0d5bdc1cdf4ff856ab35f4e1e5d09fcdd71
Paweł Wilk <wilkuwdr2008@gmail.com>
2019-12-19 13:09:37 +0100

    alert to popups change

--- public/scripts/ppFetch.js
+++ public/scripts/ppFetch.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     $('.pp_button').on('click', (e) => {
-        if($('.menu__meme__button').length == 0) return alert('Only logged users can give PP!')
+        if($('.menu__meme__button').length == 0) return $('#alert__not__logged').modal({
+            fadeDuration: 100
+        });
 
         const meme_id = $(e.currentTarget).parent().parent().parent()[0].attributes.meme_id.nodeValue;
         fetch(`meme/like/${meme_id}`,{
@@ -9,7 +11,9 @@ $(document).ready(function(){
         .then(resp => resp.json())
         .then(resp => {
             let actualLikes = 0;
-            if(resp) return alert('You already gave a PP for this meme!')
+            if(resp) return $("#alert__already__gave__pp").modal({
+                fadeDuration: 100
+            });
             actualLikes = parseInt($(e.currentTarget).find('.pp__counter')[0].innerHTML) + 1;
             $(e.currentTarget).find('.pp__counter')[0].innerHTML = actualLikes;
         })
