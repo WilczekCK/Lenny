447efb356b5edf0768f3e8960a93609ffbb5416d
Paweł Wilk <wilkuwdr2008@gmail.com>
2019-12-19 16:44:22 +0100

    header rebuilded, active added

---
+++ public/scripts/activeControl.js
@@ -0,0 +1,34 @@
+
+$('ul.filters li').click((e => {
+    const focused = $(e.currentTarget)[0].lastChild.id;
+    $('ul.filters *').removeClass('active');
+
+    switch (focused){
+        case 'std':
+            $(`a#${focused}`).addClass('active');
+            break;
+        case 'mania':
+            $(`a#${focused}`).addClass('active');
+            break;
+        case 'ctb':
+            $(`a#${focused}`).addClass('active');
+            break;
+        case 'taiko':
+            $(`a#${focused}`).addClass('active');
+            break;
+        case 'tag__finder':
+            if($(`.${focused}`).css('display') == 'none'){
+                $(`.${focused}`).fadeIn();
+                $(`#${focused}`).addClass('active');
+            }else{
+                $(`#${focused}`).addClass('active');
+                $(`#${focused}`).on('click', _ => {
+                    $(`.${focused}`).fadeOut();
+                });
+            }
+        }
+
+        if($('#tag__finder').hasClass('active') == false){$('.tag__finder').fadeOut()}
+}));
+
+
