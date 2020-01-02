2e69066b6b5974e81a2dbed77b4506b665451a44
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-01-02 14:07:23 +0100

    refactored, init

--- public/scripts/activeControl.js
+++ public/scripts/activeControl.js
@@ -1,37 +1,90 @@
-
-$('ul.filters li').click((e => {
-    const focused = $(e.currentTarget)[0].lastChild.id;
-    $('ul.filters *').removeClass('active');
-
-    switch (focused){
-        case 'std':
-            $(`a#${focused}`).addClass('active');
-            break;
-        case 'mania':
-            $(`a#${focused}`).addClass('active');
-            break;
-        case 'ctb':
-            $(`a#${focused}`).addClass('active');
-            break;
-        case 'taiko':
-            $(`a#${focused}`).addClass('active');
-            break;
-        case 'tag__finder':
-            if($(`.${focused}`).css('display') == 'none'){
-                $(`.${focused}`).fadeIn();
-                $(`#${focused}`).addClass('active');
+$(window).on('load', _ => {
+    var activeMenu = activeMenu || {};
+    activeMenu = {
+        config : {
+            menuItems: 'ul.filters li',
+            activeName: 'active',
+            tags: ['std', 'mania', 'ctb', 'taiko'],
+            finder: '#tag__finder',
+            fadeSpeed: 300
+        },
+        isItemActive: (menu_item) => {
+            if($(menu_item).hasClass(activeMenu.config.activeName)) return true
+            else return false;
+        },
+        activateItem: (menu_item) => {
+            return $(menu_item).addClass(activeMenu.config.activeName);
+        },
+        removeActiveAll: _ => {
+            $(`${activeMenu.config.menuItems} *`).removeClass('active');
+        },
+        handleTagFinder: _ => {
+                        
+        },
+        clickedMenuItem: (menu_item) => {
+            const infoAbout = $(menu_item)[0];
+            if(activeMenu.isItemActive(infoAbout)){
+                console.log('active')
             }else{
-                $(`#${focused}`).addClass('active');
-                $(`#${focused}`).on('click', _ => {
-                    $(`.${focused}`).fadeOut();
-                    setTimeout(_ => {
-                        $(`#${focused}`).removeClass('active');
-                    }, 500)
-                });
+                activeMenu.removeActiveAll();
+                activeMenu.activateItem(menu_item)
+                
+                if(menu_item === '#tag__finder'){
+                    activeMenu.handleTagFinder();
+                }
+                console.log('not active')
             }
-        }
 
-        if($('#tag__finder').hasClass('active') == false){$('.tag__finder').fadeOut()}
-}));
+            return 0;
+        },
+        init: _ => {
+            $('ul.filters li').click((e => {
+               const focused = `#${$(e.currentTarget)[0].lastChild.id}`;
+               activeMenu.clickedMenuItem(focused);
+            }))
+        }
+    }
+
+    activeMenu.init();
+})
+
+
+
+
+
+// $('ul.filters li').click((e => {
+//     const focused = $(e.currentTarget)[0].lastChild.id;
+//     $('ul.filters *').removeClass('active');
+
+//     switch (focused){
+//         case 'std': 
+//             $(`a#${focused}`).addClass('active');
+//             break;
+//         case 'mania':
+//             $(`a#${focused}`).addClass('active');
+//             break;
+//         case 'ctb':
+//             $(`a#${focused}`).addClass('active');
+//             break;
+//         case 'taiko':
+//             $(`a#${focused}`).addClass('active');
+//             break;
+//         case 'tag__finder':
+//             if($(`.${focused}`).css('display') == 'none'){
+//                 $(`.${focused}`).fadeIn();
+//                 $(`#${focused}`).addClass('active');
+//             }else{
+//                 $(`#${focused}`).addClass('active');
+//                 $(`#${focused}`).on('click', _ => {
+//                     $(`.${focused}`).fadeOut();
+//                     setTimeout(_ => {
+//                         $(`#${focused}`).removeClass('active');
+//                     }, 500)
+//                 });
+//             }
+//         }
+
+//         if($('#tag__finder').hasClass('active') == false){$('.tag__finder').fadeOut()}
+//}));
 
 
