f1203926600a714f5122477d11f533f73bca6f5b
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-01-15 13:35:15 +0100

    menu - more understandable code

--- public/scripts/isotopeConfig.js
+++ public/scripts/isotopeConfig.js
@@ -1,44 +1,53 @@
 $(window).on('load', _ => {
-  var $grid = $('.meme__container').isotope({
-    itemSelector: '.meme__item',
-    masonry: {
-      columnWidth: 50,
-      isFitWidth: true
-    }
-  })
+  var isotopeConf = isotopeConf || {};
+  isotopeConf = {
+    filterValue: '',
+    menuItem: '',
+    $grid: $('.meme__container').isotope({
+      itemSelector: '.meme__item',
+      masonry: {
+        columnWidth: 50,
+        isFitWidth: true
+      }
+    }),
+    onClick: (item) => {
+      switch (item.firstChild.localName){
+        case 'a':
+          isotopeConf.filterValue = `.${$(item).find('a')[0].id}`;
+          isotopeConf.menuItem = $(item).find('a')[0];
+          isotopeConf.tagClicked(isotopeConf.filterValue)
+          break
+        case 'i':
+          isotopeConf.filterValue = `.${$(item).find('i')[0].id}`;
+          isotopeConf.menuItem = $(item).find('i')[0];
+          break
+        case 'input':
+          break;
+      }
+    },
+    tagClicked: (filterValue) => {
+      if($(isotopeConf.menuItem).hasClass('active')){
+        return isotopeConf.$grid.isotope({ filter: '*', masonry: {columnWidth: 50} });
+      }
 
+      if(!$(isotopeConf.menuItem).hasClass('active')){
+        return isotopeConf.$grid.isotope({ filter: filterValue, masonry: {columnWidth: 50} });
+      } 
+    }
+  }
+
+
+
+  //listeners
   $('.menu__container__wrap__menu li').on('click', function () {
-    let filterValue;
-    let menuItem;
-
-    switch (this.firstChild.localName){
-      case 'a':
-        filterValue = `.${$(this).find('a')[0].id}`;
-        menuItem = $(this).find('a')[0];
-        break
-      case 'i':
-        filterValue = `.${$(this).find('i')[0].id}`;
-        menuItem = $(this).find('i')[0];
-        break
-    }
-
-    $(window).on('resize', function(){
-      imagesLoaded('.meme__container', function () {
-        $grid.isotope({ filter: filterValue, masonry: {columnWidth: 50} });
-      })
-    })
-
-    if($(menuItem).hasClass('active') && !menuItem ) return $grid.isotope({ filter: '*', masonry: {columnWidth: 50} }); //if active, reset grid
-    if(!$(menuItem).hasClass('active') ){
-      return $grid.isotope({ filter: filterValue, masonry: {columnWidth: 50} });
-    } 
+    isotopeConf.onClick(this);
   })
 
   $('#tag__finder').on('keypress', function (e) {
     if (e.which == 13) {
       const filterValue = `.${$(this)[0].value}`.toLowerCase()
-      if (filterValue == '.') return $grid.isotope({ filter: '*', masonry: {columnWidth: 50} }); //empty, reset grid
-      $grid.isotope({ filter: filterValue, masonry: {columnWidth: 50} });
+      if (filterValue == '.') return isotopeConf.$grid.isotope({ filter: '*', masonry: {columnWidth: 50} }); //empty, reset grid
+      isotopeConf.$grid.isotope({ filter: filterValue, masonry: {columnWidth: 50} });
     }
   })
 })
