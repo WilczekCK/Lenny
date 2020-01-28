ff5543ec23a038fd5a920c6e8f8d6a72496d2204
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-01-28 15:41:41 +0100

    main info page

--- routes/info.js
+++ routes/info.js
@@ -3,7 +3,9 @@ const koaBody = require('koa-body');
 
 module.exports = ({ infoRoute }) => {
     infoRoute.get('/', koaBody(), async (ctx, next) => {
-        ctx.throw('404', 'That page does not exist');
+        const is_player_logged = ctx.req.body[0];
+        await ctx.render('info', { userInfo: is_player_logged})
+        next();
     });
 
     infoRoute.get('/rules', koaBody(), async (ctx, next) => {
