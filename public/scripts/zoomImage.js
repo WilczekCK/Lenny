3306ba480864a79ac6c74dc2c6da3e4a233e55f3
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-02-04 15:26:21 +0100

    id recognize on click

--- public/scripts/zoomImage.js
+++ public/scripts/zoomImage.js
@@ -1,16 +1,22 @@
-$(window).on('load', function(){
+$(window).on('load', function () {
     var zoomImage = zoomImage || {};
-        zoomImage = {
-            config:{
-                clickableEl: $('.meme__item'),
-                fadeSpeed: 300,
-            },
-            getMemeID: function(){
-
+    zoomImage = {
+        config: {
+            clickableEl: $('.meme__item'),
+            fadeSpeed: 300,
+        },
+        getMemeID: function (clickedEl) {
+            if($(clickedEl)[0].className == 'pp__amount' || $(clickedEl)[0].className == 'meme__container') return 0;
+            
+            if ($(clickedEl).hasClass('meme__item')) {
+                return $(clickedEl).find('.pp__counter')[0].attributes.meme_id.value;
+            } else {
+                return $(clickedEl.offsetParent).find('.pp__counter')[0].attributes.meme_id.value;
             }
         }
+    }
 
-        $(zoomImage.config.clickableEl).on('click', function(e){
-            console.log(e)
-        })
+    $(document).on('click', zoomImage.config.clickableEl, function (e) {
+        if ($(e.target)) zoomImage.getMemeID(e.target);
+    })
 })
