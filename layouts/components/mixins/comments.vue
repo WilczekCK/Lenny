dba26cbdc18b7db4881c32ff5b24bbf33c5b62ba
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-07-10 14:06:41 +0200

    input for comment included with simple styling

--- layouts/components/mixins/comments.vue
+++ layouts/components/mixins/comments.vue
@@ -1,18 +1,22 @@
 <template lang="pug">
     .comment__feature
-      h3="Comments:"
       .comment__feature--list(v-if="this.comments")
+        h3="Comments:"
         .comment__feature--list__comment(v-for="comment in this.comments")
           .comment__avatar
             img(src="~/assets/img/logo.png")
           .comment__content
             .comment__content__socials
-              .comment__content__socials--author {{comment.username}} 
+              .comment__content__socials--author {{comment.username}}
               .comment__content__socials--date {{moment(comment.date)}}
             .comment__content--text  {{comment.content}}
       .comment__feature--noComments(v-else)
         h3="No comments yet - be first to do it!"
       .comment__feature--newComment
+        form
+            input(type="text" placeholder="Your reply to this meme" maxlength="100")
+            button(class="post__comment")
+                i(class='fas fa-comment')
 </template>
 
 <script>
