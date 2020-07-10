3e1402fd73a28d5603de2f95b9696c7b94afcaa8
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-07-10 16:47:50 +0200

    added old routes  to routes file

--- server/api/userRoute.js
+++ server/api/userRoute.js
@@ -1,22 +1,28 @@
+import * as auth from './controllers/auth'
+import passport from 'koa-passport'
+import FacebookStrategy from 'passport-facebook'
+import session from 'koa-session'
+
 export function printRoutes (router) {
-    const users = [
-        { name: 'Alexandre' },
-        { name: 'Pooya' },
-        { name: 'Sébastien' },
-    ]
+    auth.oAuth2.init();
 
-    return router.get('/users', async (ctx, next) => {
-            ctx.type = 'json'
-            ctx.body = users
-        }),
+    return router.get('/', passport.authenticate('facebook')),
 
-        router.get('/users/:id', async (ctx, next) => {
-            const id = parseInt(ctx.params.id)
-            console.log(users[id])
-            if (id >= 0 && id < users.length) {
-              ctx.body = users[id]
-            } else {
-              ctx.throw(404)
-            }
-        })
+    router.get('/callback', passport.authenticate('facebook'), async (ctx, next) => {
+        await auth.convertToken(ctx.session, ctx.req.user.accessToken);
+        await ctx.redirect('success')
+    }),
+
+    router.get('/success', async (ctx, next) => {
+        await ctx.render('pages/login_success');        
+    }),
+
+    router.get('/out', async (ctx, next) => {
+        await auth.logout(ctx);
+        await ctx.redirect('/');
+    }),
+
+    router.get('/failed', async (ctx, next) => {
+        ctx.throw(400, 'Error while logging')
+    })
 } 
