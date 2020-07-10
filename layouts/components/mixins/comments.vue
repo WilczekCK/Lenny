f517e29d7c6a1ac66d9eab70b232ac10ccd8fe1a
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-07-10 12:55:30 +0200

    component of comments, logic, first html structure

---
+++ layouts/components/mixins/comments.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+    .comment__feature
+      .comment__feature--list(v-if="this.comments")
+        .comment__feature--list__comment(v-for="comment in this.comments")
+          .comment__author {{comment.username}}
+          .comment__date {{moment(comment.date)}}
+          .comment__content {{comment.content}}
+      .comment__feature--noComments(v-else)
+        h2="No comments yet"
+      .comment__feature--newComment
+</template>
+
+<script>
+import moment from "moment";
+export default {
+  props: ['commentList'],
+  data: function() {
+    return {
+      comments: this.commentList
+    }
+  },
+  methods: {
+    moment: function(date){
+        const today = moment();
+        const incomingDate = moment(date);
+        return " · "+incomingDate.from(today);
+    }
+  }
+}
+</script>
