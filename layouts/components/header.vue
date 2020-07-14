cf6fbdd33cba2883d57816a315625d4142e484f5
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-07-14 12:24:17 +0200

    css changes, added icons

--- layouts/components/header.vue
+++ layouts/components/header.vue
@@ -6,10 +6,15 @@
           span!="Semes<br>Koa + Nuxt meme template"
         .header__wrapper__menuItem--searchBar
           input(type="text" placeholder="Browse by #tags")
-          a(href="/api/login" v-if="!this.user").header__wrapper__menuItem--accountManagement="Login using facebook"
-          a(v-else)
-            h6="LOGGED!"
-            h6(@click="logout()")="logout"
+        .header__wrapper__menuItem--accountManagement
+          a(href="/api/login" v-if="!this.user")="Login using facebook"
+          a(class="header__wrapper__menuItem--accountManagement-logged" v-else)
+            h3='Hello {{this.user.username}}'
+            .header__wrapper__menuItem--accountManagement-logged-settings
+              i(class="fas fa-list")
+              i(class="fas fa-user-cog")
+              a(@click="logout()")
+                i(class="fas fa-sign-out-alt" )
 
 </template>
 
