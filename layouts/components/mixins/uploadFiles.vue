b880f22074c9fdbca1bc7b321ca349a824dff7b9
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-07-20 15:57:29 +0200

    image is displayed, 0 css

--- layouts/components/mixins/uploadFiles.vue
+++ layouts/components/mixins/uploadFiles.vue
@@ -4,6 +4,7 @@
         input(type="file" ref="file" @change="selectFile")
         button(class="btn btn-success" :disabled="!selectedFiles" @click.prevent="upload")="Upload"
         p {{ message }}
+        img(v-if="base64image" :src="base64image")
 </template>
 
 <script>
@@ -15,6 +16,7 @@ export default {
     return {
         selectedFiles: undefined,
         currentFile: undefined,
+        base64image: undefined,
         progress: 0,
         message: "",
 
@@ -24,7 +26,17 @@ export default {
     methods: {
         selectFile(e) {
             this.selectedFiles = e.target.files[0];
+            this.createBase64Image(this.selectedFiles)
             return this.selectedFiles;
+        },
+        createBase64Image(fileObject){
+            const reader = new FileReader();
+
+            reader.onload = (e) => {
+                this.base64image = e.target.result;
+            }
+
+            reader.readAsDataURL(fileObject)
         },
         upload() {
             this.progress = 0;
@@ -32,12 +44,12 @@ export default {
 
             UploadService.upload(this.currentFile, event => {
                 this.progress = Math.round((100 * event.loaded) / event.total);
+                this.base64decod = reader.readAsText(this.currentFile)
             })
             .then(response => {
                 this.message = response.data.message;
             })
             .catch((e) => {
-                console.log('error '+e)
                 this.progress = 0;
                 this.message = "Could not upload the file!";
                 this.currentFile = undefined;
