a17b2b5afdf488c85967dc04424f2511e25aeb83
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-07-31 16:01:13 +0200

    unified modals

---
+++ layouts/components/modals/modal.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+    transition(name="modal" v-if="this.modalState")
+        .modal__mask()
+            .modal__mask__wrapper
+                .modal__mask__wrapper__container
+                    button(class="modal-default-button" @click="changeState()")
+                        i(class="fas fa-times")
+                    .modal__mask__wrapper__container--header
+                    .modal__mask__wrapper__container--body
+                        formComp
+                    .modal__mask__wrapper__container--footer
+</template>
+
+<script>
+import axios from 'axios'
+import formComp from "../forms/main.vue";
+export default {
+    components: {
+        formComp
+    },
+    data: function(){
+        return{
+            modalState: this.$store.state.modalState
+        }
+    },
+    watch: {
+        storeModalState: function(newVal, oldVal){
+            this.modalState = newVal;
+        }
+    },
+    computed: {
+        storeModalState: function(){
+            return this.$store.state.modalState;
+        }
+    },
+    methods:{
+        changeState: function(){
+            this.$store.commit('modalToggle', 'avatar');
+            this.modalState = this.$store.state.modalState;
+        }
+    },
+
+}
+</script>
+
+<style>
+
+</style>
