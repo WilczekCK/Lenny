32a136732029b3073dc6ee10a7de43a087a8249e
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-10-01 16:13:59 +0200

    file init

---
+++ layouts/components/mixins/admin-tools.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+    .admin_tools_wrapper
+      .admin_tools_container
+        ul
+          li="Ban Author"
+          li="Remove Meme"
+          li=""
+</template>
+
+<script>
+import Vue from 'vue';
+export default {
+  data: function() {
+    return {
+    }
+  },
+  mounted: async function () {
+  },
+  methods: {
+
+  }
+}
+</script>
