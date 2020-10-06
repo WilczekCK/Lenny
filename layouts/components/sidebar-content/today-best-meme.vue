722076e40936ad8027c7c364b091017826e4ac2c
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-10-06 16:47:28 +0200

    axios import, axios method on today best meme

--- layouts/components/sidebar-content/today-best-meme.vue
+++ layouts/components/sidebar-content/today-best-meme.vue
@@ -6,16 +6,26 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   data: function() {
     return{
         isPageLoaded:false,
     }
   },
-  mounted: function () {
-
-  },
+  mounted: async function () {
+    this.todayBestMeme = this.getTodayBestMeme();
+    this.isPageLoaded = true;
+  },    
   methods: {
+    getTodayBestMeme: async function () {
+        await axios({
+            url: '/api/meme/tbm',
+            method: 'GET'
+        }).then(({data}) => {
+            console.log(data)
+        })
+    }
   }
 }
 </script>
