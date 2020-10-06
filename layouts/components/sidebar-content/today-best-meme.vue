54752499ed89fe615405a901a3e4f44107ede8ee
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-10-06 16:39:10 +0200

    included the today-best-meme component in sidebar

---
+++ layouts/components/sidebar-content/today-best-meme.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+    .tbm__container
+        h4="Today best meme:"
+        .tbm__container__content(v-if="isPageLoaded")
+        .lol(v-else)="Loading..."
+</template>
+
+<script>
+export default {
+  data: function() {
+    return{
+        isPageLoaded:false,
+    }
+  },
+  mounted: function () {
+
+  },
+  methods: {
+  }
+}
+</script>
--- layouts/components/sidebar.vue
+++ layouts/components/sidebar.vue
@@ -1,13 +1,17 @@
 <template lang="pug">
     .sidebar()
       .sidebar__wrapper
-        h3="Here will be ads, highlighted memes and more"
-        span="Lorem ipsum dolor sit amet, consectetur adipiscing"
+        today_best_meme
 </template>
 
 <script>
+import today_best_meme from './sidebar-content/today-best-meme.vue'
 export default {
+  components: {
+    today_best_meme
+  },
   mounted: function () {
+
   },
   methods: {
   }
