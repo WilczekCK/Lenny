6bd965ecb1775bc3c6e8c9d4dd65af166ca85bc7
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-10-07 13:15:41 +0200

    added npm for google ads + ads content in sidebar

--- layouts/components/sidebar.vue
+++ layouts/components/sidebar.vue
@@ -2,16 +2,19 @@
     .sidebar()
       .sidebar__wrapper
         today_best_meme
+        ads
         recently_added_meme
 </template>
 
 <script>
 import today_best_meme from './sidebar-content/today-best-meme.vue'
 import recently_added_meme from './sidebar-content/recently-added-meme.vue'
+import ads from './sidebar-content/ads.vue'
 export default {
   components: {
     today_best_meme,
-    recently_added_meme
+    recently_added_meme,
+    ads
   },
   mounted: function () {
   },
