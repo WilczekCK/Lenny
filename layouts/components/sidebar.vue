3e15921f7e8f15c477af7c361267e9dd41029036
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-10-07 11:11:40 +0200

    added component recently added meme for sidebar

--- layouts/components/sidebar.vue
+++ layouts/components/sidebar.vue
@@ -2,13 +2,16 @@
     .sidebar()
       .sidebar__wrapper
         today_best_meme
+        recently_added_meme
 </template>
 
 <script>
 import today_best_meme from './sidebar-content/today-best-meme.vue'
+import recently_added_meme from './sidebar-content/recently-added-meme.vue'
 export default {
   components: {
-    today_best_meme
+    today_best_meme,
+    recently_added_meme
   },
   mounted: function () {
   },
