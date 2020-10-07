c953b382dec4d36365dcaccb4e62ef3e0391b129
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-10-07 08:17:44 +0200

    sidebar displays today best meme

--- layouts/components/sidebar-content/today-best-meme.vue
+++ layouts/components/sidebar-content/today-best-meme.vue
@@ -2,31 +2,33 @@
     .tbm__container
         h4="Today best meme:"
         .tbm__container__content(v-if="isPageLoaded")
+            .meme__container(v-if="todayBestMeme")
+                memeItem(:memeDetails="todayBestMeme")
+            .no(v-else)="No best memes for today :("
         .lol(v-else)="Loading..."
 </template>
 
 <script>
 import axios from 'axios';
+import memeItem from '../mixins/meme-item.vue';
 export default {
+  components: {
+      memeItem
+  },
   data: function() {
     return{
         isPageLoaded:false,
+        todayBestMeme: undefined,
     }
   },
-  mounted: async function () {
-    this.todayBestMeme = this.getTodayBestMeme();
-    this.isPageLoaded = true;
-  },    
-  methods: {
-    getTodayBestMeme: async function () {
-        await axios({
-            url: '/api/meme/tbm',
-            method: 'GET'
-        }).then(({data}) => {
-            var {id} = data.data;
-            return id;
-        })
-    }
-  }
+  async mounted() {
+    await axios({
+        url: '/api/meme/tbm',
+        method: 'GET'
+    }).then(({data}) => {
+        this.todayBestMeme = data.data[0];
+        this.isPageLoaded = true;
+    })
+}
 }
 </script>
