1970d201e7a287cd7a61bb084f2ff9ea97b1104d
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-10-29 13:24:38 +0100

    main /user/ route without id redirection to index page

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,45 +1,11 @@
-<template>
-  <section class="container">
-    <h1 class="title">
-      USERS
-    </h1>
-    <ul class="users">
-      <li v-for="(user, index) in users" :key="index" class="user">
-        <nuxt-link :to="'users/' + index">
-          {{ user.name }}
-        </nuxt-link>
-      </li>
-    </ul>
-  </section>
+<template lang="pug">
+  .meme__container
 </template>
 
 <script>
 export default {
-  async asyncData () {
-    let { data } = await axios.get('/api/users')
-    return { users: data.data }
-  },
-  head () {
-    return {
-      title: 'Users'
-    }
+  mounted() {
+    this.$router.push(`/`)
   }
 }
 </script>
-
-<style scoped>
-.title
-{
-  margin: 30px 0;
-}
-.users
-{
-  list-style: none;
-  margin: 0;
-  padding: 0;
-}
-.user
-{
-  margin: 10px 0;
-}
-</style>
